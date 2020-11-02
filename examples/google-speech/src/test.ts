12ce8a44a0bc35529c899cc98254b1f685b9b6fa
SketchingDev <31957045+SketchingDev@users.noreply.github.com>
2020-11-02 20:44:28 +0000

    Example

--- examples/google-speech/src/test.ts
+++ examples/google-speech/src/test.ts
@@ -4,7 +4,7 @@ import {
   doNothing,
   IvrTest,
   ordered,
-  press,
+  press, similarTo,
   testRunner,
   TestSubject,
 } from "ivr-tester";
@@ -19,13 +19,13 @@ const call: TestSubject = {
 
 const test: IvrTest = {
   name: "Pressing 4 exits the flow",
-  test: ordered([
+  test: inOrder([
     {
-      when: contains("this will allow you to adjust call recording behaviour"),
+      whenTranscript: contains("this will allow you to adjust call recording behaviour"),
       then: press("4"),
     },
     {
-      when: contains("thanks for calling"),
+      whenTranscript: hasPart(similarTo("thanks for calling")),
       then: doNothing(),
     },
   ]),
