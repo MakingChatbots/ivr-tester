fc4733bdb66df7feb6030d8d28fb58ecabb79ee2
SketchingDev <31957045+SketchingDev@users.noreply.github.com>
2021-05-23 15:57:41 +0100

    Log error from catch rather than rethrowing

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -2,6 +2,11 @@
 
 import { createCli } from "./cli";
 
-createCli()(process.argv).catch((error) => {
-  throw error;
-});
+createCli()(process.argv)
+  .then(() => process.exit(0))
+  .catch((error) => {
+    if (error) {
+      console.error(error);
+    }
+    process.exit(1);
+  });
